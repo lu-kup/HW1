0a267f8d2836999e7c83ce438443777f3c28ea84
genetics <luk.kuprys@gmail.com>
2026-03-15 16:55:36 +0200

    updates

--- script.sh
+++ script.sh
@@ -23,3 +23,38 @@ samtools view -bS mapping_outputs/*.sam > mapping_outputs/*.bam
 for f in mapping_outputs/*.sam; do
   samtools view -bS "$f" > "${f%.sam}.bam"
 done
+
+for f in mapping_outputs/*.bam; do
+  samtools sort "$f" -o "${f%.bam}.sorted.bam"
+done
+
+samtools index -@ 8 -M mapping_outputs/*.sorted.bam
+
+mkdir mapped_quality_assesment
+
+for f in mapping_outputs/*.sorted.bam; do
+  samtools coverage "$f" > "${f%.bam}_coverage.out"
+done
+
+mkdir outputs
+mv mapping_outputs/*.out outputs
+
+wget https://sourceforge.net/projects/rseqc/files/BED/Human_Homo_sapiens/hg38_GENCODE_V47.bed.gz/download
+cp download raw_data/hg38_GENCODE_V47.bed.gz
+rm download
+
+gunzip raw_data/hg38_GENCODE_V47.bed.gz
+geneBody_coverage.py -r raw_data/hg38_GENCODE_V47.bed -i mapping_outputs/*.sorted.bam -o outputs/gene_body_coverage
+inner_distance.py -r raw_data/hg38_GENCODE_V47.bed -i mapping_outputs/*.sorted.bam -o outputs/inner_distance
+clipping_profile.py -s "PE" -i mapping_outputs/*.sorted.bam -o outputs/clipping_profile
+junction_annotation.py -r raw_data/hg38_GENCODE_V47.bed -i mapping_outputs/*.sorted.bam -o outputs/junction_annotation
+infer_experiment.py -r raw_data/hg38_GENCODE_V47.bed -i mapping_outputs/*.sorted.bam > outputs/infer_experiment.out
+
+multiBamSummary bins --bamfiles mapping_outputs/*.sorted.bam -o bins_results.npz
+plotCorrelation -in bins_results.npz -c spearman -p heatmap -o outputs/correlation_heatmap.png
+plotPCA -in bins_results.npz -o outputs/pca.png
+
+multiqc outputs
+
+featureCounts -T 8 -a references/gencode.v49.annotation.gtf.gz -o outputs/feature_counts.out \
+-s 0 -p -t exon -g gene_id mapping_outputs/*.sorted.bam
